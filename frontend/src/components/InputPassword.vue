4589f4140c5169727b6b5d3b86aca921c1bd56ee
Gabriel Franco <gabriel.franco@gfldev.com>
2025-01-16 19:24:57 -0300

    v0.2.2: componentes de inputs separados, inicio da lógica de login

---
+++ frontend/src/components/InputPassword.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import { PhEye, PhEyeClosed } from '@phosphor-icons/vue'
+import { onMounted, ref, watchEffect } from 'vue'
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    required: true,
+  },
+  leftInnerIcon: Object,
+  disabled: Boolean,
+  required: Boolean,
+  showable: Boolean,
+})
+
+const model = defineModel<string | null | undefined>()
+const visible = ref<boolean>(false)
+
+onMounted(() => {
+  watchEffect(() => {
+    if (props.disabled) {
+      visible.value = false
+    }
+  })
+})
+</script>
+
+<template>
+  <div class="relative mt-2 flex">
+    <div class="relative -mr-px grid grow grid-cols-1">
+      <input
+        name="passwd"
+        id="passwd"
+        :class="`peer col-start-1 row-start-1 block w-full bg-white py-1.5 ${leftInnerIcon ? 'pl-10' : 'pl-3'} ${showable ? 'rounded-l-md' : 'rounded-md'} pr-3 text-base text-gray focus:text-dark outline outline-1 -outline-offset-1 outline-gray placeholder:text-gray focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-dark sm:text-sm/6`"
+        :placeholder="placeholder"
+        v-model="model"
+        :disabled="disabled"
+        :type="showable ? (visible ? 'text' : 'password') : 'password'"
+      />
+      <label
+        for="passwd"
+        class="absolute -top-2 left-2 inline-block rounded-lg bg-white px-1 text-xs font-medium text-gray peer-focus:text-dark"
+        >{{ label }}<b v-if="required">&nbsp;*</b></label
+      >
+      <component
+        v-if="leftInnerIcon"
+        :is="leftInnerIcon"
+        class="pointer-events-none col-start-1 row-start-1 ml-3 size-5 self-center text-gray peer-focus:text-dark"
+        aria-hidden="true"
+      />
+    </div>
+    <button
+      type="button"
+      v-if="showable"
+      class="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-dark outline outline-1 -outline-offset-1 outline-gray transition-colors duration-200 hover:bg-gray hover:bg-opacity-30 focus:relative focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-dark"
+      :disabled="disabled"
+      @click="visible = !visible"
+    >
+      <PhEyeClosed v-if="visible" class="-ml-0.5 size-4 text-dark" aria-hidden="true" />
+      <PhEye v-else class="-ml-0.5 size-4 text-dark" aria-hidden="true" />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+*:disabled {
+  @apply opacity-50;
+}
+
+*:enabled {
+  @apply opacity-100;
+}
+</style>
--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import {
-  PhEyeClosed,
-  PhEye,
-  PhSignIn,
-  PhCircleNotch,
-  PhIdentificationCard,
-  PhPassword,
-} from '@phosphor-icons/vue'
+import { PhSignIn, PhCircleNotch, PhIdentificationCard, PhPassword } from '@phosphor-icons/vue'
+import InputText from '@/components/InputText.vue'
+import InputPassword from '@/components/InputPassword.vue'
+import type { LoginRequest } from '@/@types/Requests.ts'
 
 const visible = ref<boolean>(false)
 const username = ref<string | null>()
@@ -16,9 +12,33 @@ const loading = ref<boolean>(false)
 
 // Função para gerenciar o envio do formulário para autenticação do usuário
 async function handleSignIn(): Promise<void> {
+  if (!username.value || !passwd.value) {
+    // TODO: Manipular erro
+    return
+  }
+
   visible.value = false
   loading.value = true
-  // TODO: Criar lógica
+
+  const body: LoginRequest = {
+    username: username.value,
+    password: passwd.value,
+  }
+
+  const res = await fetch('https://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .finally(() => (loading.value = false))
+
+  console.log(res)
 }
 </script>
 
@@ -26,75 +46,31 @@ async function handleSignIn(): Promise<void> {
   <div
     class="flex w-full flex-col items-center justify-center gap-6 rounded-none bg-white px-8 py-4 drop-shadow-2xl sm:mx-16 sm:w-fit sm:rounded-lg sm:px-16 sm:py-8"
   >
-    <p class="text-center text-lg font-light text-agros-gray-dark">
+    <p class="text-agros-gray-dark text-center text-lg font-light">
       Por favor, preencha os campos para fazer o login.
     </p>
     <section class="flex w-full flex-col justify-center gap-6 px-8">
-      <!-- Input de usuário -->
-      <div class="relative mt-2 grid grid-cols-1">
-        <input
-          type="text"
-          name="username"
-          id="username"
-          class="text-dark outline-gray placeholder:text-gray focus:outline-dark peer col-start-1 row-start-1 block w-full rounded-md bg-white py-1.5 pl-10 pr-3 text-base outline outline-1 -outline-offset-1 focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-          placeholder="Nome de usuário"
-          v-model="username"
-          required
-          :disabled="loading"
-        />
-        <label
-          for="username"
-          class="text-gray peer-focus:text-dark absolute -top-2 left-2 inline-block rounded-lg bg-white px-1 text-xs font-medium"
-          >Usuário <b>*</b></label
-        >
-        <PhIdentificationCard
-          class="text-gray peer-focus:text-dark pointer-events-none col-start-1 row-start-1 ml-3 size-5 self-center"
-          aria-hidden="true"
-        />
-      </div>
-      <!-- Input de senha -->
-      <div class="relative mt-2 flex">
-        <div class="relative -mr-px grid grow grid-cols-1">
-          <input
-            name="passwd"
-            id="passwd"
-            class="text-dark outline-gray placeholder:text-gray focus:outline-dark peer col-start-1 row-start-1 block w-full rounded-l-md bg-white pr-3 pl-10 py-1.5 text-base outline outline-1 -outline-offset-1 focus:outline focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
-            placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-            v-model="passwd"
-            :disabled="loading"
-            :type="visible ? 'text' : 'password'"
-          />
-          <label
-            for="passwd"
-            class="text-gray peer-focus:text-dark absolute -top-2 left-2 inline-block rounded-lg bg-white px-1 text-xs font-medium"
-            >Senha <b>*</b></label
-          >
-          <PhPassword
-            class="text-gray peer-focus:text-dark pointer-events-none col-start-1 row-start-1 ml-3 size-5 self-center"
-            aria-hidden="true"
-          />
-        </div>
-        <button
-          type="button"
-          class="text-dark outline-gray hover:bg-gray focus:outline-dark flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold outline outline-1 -outline-offset-1 transition-colors duration-200 hover:bg-opacity-30 focus:relative focus:outline focus:outline-2 focus:-outline-offset-2"
-          :disabled="loading"
-          @click="visible = !visible"
-        >
-          <PhEyeClosed v-if="visible" class="text-dark -ml-0.5 size-4" aria-hidden="true" />
-          <PhEye v-else class="text-dark -ml-0.5 size-4" aria-hidden="true" />
-        </button>
-      </div>
+      <InputText
+        label="Usuário"
+        placeholder="Nome de usuário"
+        v-model="username"
+        :disabled="loading"
+        :left-inner-icon="PhIdentificationCard"
+        required
+      />
+      <InputPassword
+        label="Senha"
+        placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+        v-model="passwd"
+        :disabled="loading"
+        :left-inner-icon="PhPassword"
+        showable
+        required
+      />
     </section>
-    <!-- Botão de login -->
     <button
       type="button"
-      :class="
-        'text-dark focus-visible:outline-dark enabled:hover:bg-dark ' +
-        ' focus-visible:outline-offset inline-flex items-center gap-x-2 rounded-md px-3 py-1.5 text-base shadow-sm' +
-        ' transition duration-200 ease-in-out focus:-outline-offset-2 focus-visible:outline focus-visible:outline-2' +
-        ' enabled:hover:text-white' +
-        (loading ? ' disabled:bg-dark disabled:text-white' : '')
-      "
+      :class="`text-dark focus-visible:outline-dark ${loading ? 'disabled:bg-dark disabled:text-white' : ''} focus-visible:outline-offset inline-flex items-center gap-x-2 rounded-md px-3 py-1.5 text-base shadow-sm transition duration-200 ease-in-out focus:-outline-offset-2 focus-visible:outline focus-visible:outline-2 enabled:hover:bg-dark enabled:hover:text-white`"
       :disabled="!username || !passwd || loading"
       @click="handleSignIn"
     >
